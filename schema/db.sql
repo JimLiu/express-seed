d9ef736a2e45ee5270eef82b189f7ab41da02559
Jim Liu <junminliu@gmail.com>
2015-08-27 02:42:50 -0500

    update schema

--- schema/db.sql
+++ schema/db.sql
@@ -1,25 +1,28 @@
 
 -- Users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(254) NOT NULL,
-  `password` varchar(16) NOT NULL,
+  `password` varchar(256) NOT NULL DEFAULT '',
   `screen_name` varchar(64) NOT NULL DEFAULT '',
-  `created` int(10) UNSIGNED NOT NULL, 
-  `banned_until` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  PRIMARY KEY  (`id`),
+  `avatar_url` varchar(256) DEFAULT NULL,
+  `created` int(10) unsigned NOT NULL,
+  `banned_until` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `user_tokens` (
   `user_id` int(11) unsigned NOT NULL,
   `token` varchar(40) NOT NULL,
+  `type` varchar(16) NOT NULL DEFAULT 'signin',
   `created` int(10) unsigned NOT NULL,
   `expires` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`user_id`),
+  PRIMARY KEY (`user_id`,`token`),
   UNIQUE KEY `uniq_token` (`token`),
   KEY `k_user_id` (`user_id`),
-  CONSTRAINT `fk_user_tokens_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `k_expires` (`expires`),
+  CONSTRAINT `fk_user_tokens_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tags` (
